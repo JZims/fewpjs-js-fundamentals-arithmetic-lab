0a1cab163f5caf07fca645f5a235df5b726c9798
JZims <JZimmsGames@gmail.com>
2021-03-15 21:01:25 -0400

    completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+var num1 = 31;
+var num2 = 2; 
+var multiply = (num1 * num2);
+console.log(multiply);
+
+var random = Math.ceil(Math.random() *10) +1;
+
+var num3 = 9;
+var num4 = 5;
+
+var mod = num3 % num4;
+
+var max= Math.max(3, 5, 15, 20);
